ed874e8d026b1474558af01fa213c1f3eb892823
neuqliu <1196932066@qq.com>
2016-10-31 11:18:04 +0800

    增加SEO关键字，增加百度统计

--- application/views/common_head.php
+++ application/views/common_head.php
@@ -6,10 +6,18 @@
     <title>柠檬本色</title>
 <?php endif; ?>
 <meta name="description" content="柠檬本色">
-<meta name="keywords" content="index">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <meta name="renderer" content="webkit">
 <meta http-equiv="Cache-Control" content="no-siteapp" />
 <link rel="shortcut icon" href="/assets/img/favicon.ico">
 <link rel="stylesheet" href="/assets/third_party/amazeui/css/amazeui.min.css"/>
 <link rel="stylesheet" href="/assets/css/main.css?r=<?= time(); ?>">
+<script>
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "//hm.baidu.com/hm.js?3c8023f1df3884ecb9c5138ff126e85c";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+</script>
