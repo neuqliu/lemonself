2617bb526201ecdb54a1809a25b8f17de66cfbe8
neuqliu <1196932066@qq.com>
2016-10-31 20:34:19 +0800

    修改session重新生成时间

--- application/config/config.php
+++ application/config/config.php
@@ -373,7 +373,7 @@ $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
+$config['sess_time_to_update'] = 7200;
 $config['sess_regenerate_destroy'] = FALSE;
 
 /*
